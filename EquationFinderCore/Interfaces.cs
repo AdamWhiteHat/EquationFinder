165a88cf3d6223c49c9d5f0faa66f95a60406b04
Adam White <arakaska@gmail.com>
2015-10-21 03:25:06 -0600

    Interfaces.cs file was missing!

---
+++ EquationFinderCore/Interfaces.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *
+ * Developed by Adam Rakaska
+ *  http://www.csharpprogramming.tips
+ * 
+ */
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EquationFinderCore
+{
+	public delegate void FoundEquationDelegate(string resultsObject);
+
+	public interface IEquation
+	{
+		void SetArgs(IEquationFinderArgs args);
+		void GenerateNewAndEvaluate();
+		bool IsSolution { get; }
+		decimal Result { get; }
+		string ToString();
+	}
+
+	public interface IEquationFinderArgs
+	{
+		string TermPool { get; }
+		string OperatorPool { get; }
+		decimal TargetValue { get; }
+		int NumberOfOperations { get; }
+		Random Rand { get; }
+	}	
+}
