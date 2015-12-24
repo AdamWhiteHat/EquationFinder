c244215714e78a7ed717b08fac2e62d9471bed0c
Adam White <arakaska@gmail.com>
2015-12-24 15:44:58 -0700

    Cleaned up StaticScriptControl.

--- EquationFactories/AlgebraicString/StaticScriptControl.cs
+++ EquationFactories/AlgebraicString/StaticScriptControl.cs
@@ -4,44 +4,23 @@
  *  http://www.csharpprogramming.tips
  * 
  */
-using System;
-using System.Linq;
-using MSScriptControl;
-using System.Threading;
-using System.Collections.Generic;
-
-using EquationFinderCore;
-
 namespace EquationFactories
-{
+{	
 	public sealed class StaticScriptControl
 	{
 		public static MSScriptControl.ScriptControl Instance { get { return Nested.instance; } }
+		private StaticScriptControl() { }
 
-		private StaticScriptControl()
+		private sealed class Nested
 		{
-		}
-
-		private class Nested
-		{
-			internal static readonly MSScriptControl.ScriptControl instance;
-			static Nested() // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit
-			{ 
-				instance = new MSScriptControl.ScriptControl();
-				instance.AllowUI = false;
-				instance.Language = "VBScript";
-			}
+			internal static readonly MSScriptControl.ScriptControl instance = new MSScriptControl.ScriptControl() { AllowUI = false, Language = "VBScript" };
+			static Nested() { }  // Explicit static constructor to tell C# compiler not to mark type as beforefieldinit			
+			private Nested() { } // Cannot be instantiated
 		}
 
 		public static decimal Evaluate(string Equation)
-		{
-			return HelperClass.String2Decimal(EvaluateToString(Equation));
-		}
-
-		public static string EvaluateToString(string Equation)
-		{
-			object result = Instance.Eval(Equation);
-			return result.ToString();
+		{			
+			return EquationFinderCore.HelperClass.String2Decimal(Instance.Eval(Equation).ToString());
 		}
 	}
 }
